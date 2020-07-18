c1e58af766822171b43996edfbb38f37d24e1c54
Robin Kurosawa <rkuro@bu.edu>
2020-07-18 15:48:54 -0400

    finished ifs,functions,prims,structs,quiz :)

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,10 +7,14 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
-
-// Put your function here!
-// fn ..... {
+const apple_price_normal: i32 = 2;
+const apple_price_sale: i32 = 1;
+fn calculate_apple_price(num_apples: i32) -> i32 {
+    if num_apples > 40 {
+        return num_apples * apple_price_sale;
+    }
+    return num_apples * apple_price_normal;
+}
 
 // Don't modify this function!
 #[test]
